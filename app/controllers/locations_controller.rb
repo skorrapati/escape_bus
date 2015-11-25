8d7355a0ad37908716d94d7334ef3ec160b6cef1
Sia <siakaramalegos@gmail.com>
2015-11-25 10:22:30 -0600

    Searches for nearby buses and returns bus info to location show view

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,5 @@
 class LocationsController < ApplicationController
+  include LocationsHelper # module with our helper methods
   before_action :set_location, only: [:show, :edit, :update, :destroy]
 
   # GET /locations
@@ -10,6 +11,27 @@ class LocationsController < ApplicationController
   # GET /locations/1
   # GET /locations/1.json
   def show
+    # MARTA API URL
+    source = 'http://developer.itsmarta.com/BRDRestService/RestBusRealTimeService/GetAllBus'
+
+    # Use our helper method to parse the data from the API
+    @buses = fetch_api_data(source)
+
+    # Loop through all buses and return only those that are nearby
+    @nearby_buses = []
+
+    @buses.each do |bus|
+      user_lat = @location.latitude
+      user_long = @location.longitude
+      bus_lat = bus['LATITUDE'].to_f
+      bus_long = bus['LONGITUDE'].to_f
+
+      if is_nearby?(user_lat, user_long, bus_lat, bus_long)
+        @nearby_buses << bus
+      end
+    end
+
+    # TODO: If no buses, return with a notice and redirect to the new or edit page
   end
 
   # GET /locations/new
--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,15 @@
 module LocationsHelper
+  # Parse the API data to store it in an array of hashes so that each bus is a hash.
+  def fetch_api_data(source)
+    response = Net::HTTP.get_response(URI.parse(source))
+    data = response.body
+    JSON.parse(data)
+  end
+
+  # Compare the user lat/long with a bus to see if it is "nearby" which for now is 0.01 degress of lat/long.
+  # TODO: Make this a true circular radius
+  # TODO: Give user option of how close
+  def is_nearby?(user_lat, user_long, bus_lat, bus_long)
+    (user_lat - bus_lat).abs <= 0.01 && (user_long - bus_long).abs <= 0.01
+  end
 end
